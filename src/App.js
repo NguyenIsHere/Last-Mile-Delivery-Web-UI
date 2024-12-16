46ef4abe781b40aaee1dec8857dab1cbd425e1fc
NguyenIsHere <22520988@gm.uit.edu.vn>
2024-12-16 23:21:07 +0700

    Initialize grab home header layout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import MainLayout from './MainLayout'
 import Home from './pages/Home/Home'
+import GrabHome from './pages/GrabHome/GrabHome'
 import './App.css'
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/grab' element={<GrabHome />} />
         <Route path='/main/*' element={<MainLayout />} />
       </Routes>
     </Router>
